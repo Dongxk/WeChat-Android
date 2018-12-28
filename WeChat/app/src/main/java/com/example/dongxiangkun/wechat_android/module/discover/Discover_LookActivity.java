9a44947e63b6f44307dd0084ba97a36c9a663921
董向坤 <1043643016@qq.com>
2018-12-28 17:10:28 +0800

    看一看

--- WeChat/app/src/main/java/com/example/dongxiangkun/wechat_android/module/discover/Discover_LookActivity.java
+++ WeChat/app/src/main/java/com/example/dongxiangkun/wechat_android/module/discover/Discover_LookActivity.java
@@ -1,15 +1,27 @@
 package com.example.dongxiangkun.wechat_android.module.discover;
 
+import android.os.Handler;
+import android.support.annotation.MainThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.dongxiangkun.wechat_android.R;
 import com.example.dongxiangkun.wechat_android.base.NavigationBar;
+import com.example.dongxiangkun.wechat_android.model.LookModel;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -21,6 +33,8 @@ public class Discover_LookActivity extends AppCompatActivity {
 
     private ListView listView;
     private NavigationBar navigation;
+    private List<LookModel> datalist;
+    private LookAdapter lookAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +48,62 @@ public class Discover_LookActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        datalist = new ArrayList<>();
+
         navigation.setLeftImage(R.mipmap.back_icon);
 
-
         listView = findViewById(R.id.discover_look_listview);
-        LookAdapter lookAdapter = new LookAdapter(this);
+        lookAdapter = new LookAdapter(this, datalist);
         listView.setAdapter(lookAdapter);
+
+
+        getData();
+    }
+
+    public void getData(){
+
+        String url  = "http://c.m.163.com/nc/article/list/T1348649079062/0-10.html";
+        OkHttpClient okHttpClient = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .header("User-Agent", "")
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(Discover_LookActivity.this, "网络错误", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                JsonObject jsonObject = new JsonParser().parse(response.body().string()).getAsJsonObject();
+                JsonArray jsonArray = jsonObject.getAsJsonArray("T1348649079062");
+                Gson gson = new Gson();
+                for (JsonElement jsonModel : jsonArray){
+                    LookModel lookModel = gson.fromJson(jsonModel, new TypeToken<LookModel>(){}.getType());
+                    datalist.add(lookModel);
+                }
+
+
+                new Handler(getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        lookAdapter.notifyDataSetChanged();
+                    }
+                });
+
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        lookAdapter.notifyDataSetChanged();
+//                    }
+//                });
+
+            }
+        });
+
     }
 }
--- WeChat/app/src/main/java/com/example/dongxiangkun/wechat_android/module/discover/LookAdapter.java
+++ WeChat/app/src/main/java/com/example/dongxiangkun/wechat_android/module/discover/LookAdapter.java
@@ -36,41 +36,10 @@ public class LookAdapter extends BaseAdapter {
     private List<LookModel> dataList;
     private Context lContext;
 
-    public LookAdapter(Context context) {
+    public LookAdapter(Context context, List<LookModel> dataList) {
 
-        dataList = new ArrayList<LookModel>();
        lContext = context;
-       getData();
-    }
-
-    public void getData(){
-
-        String url  = "http://c.m.163.com/nc/article/list/T1348649079062/0-10.html";
-        OkHttpClient okHttpClient = new OkHttpClient();
-        final Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .header("User-Agent", "")
-                .build();
-        Call call = okHttpClient.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Toast.makeText(lContext, "网络错误", Toast.LENGTH_SHORT).show();
-            }
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-
-                JsonObject jsonObject = new JsonParser().parse(response.body().string()).getAsJsonObject();
-                JsonArray jsonArray = jsonObject.getAsJsonArray("T1348649079062");
-                Gson gson = new Gson();
-                for (JsonElement jsonModel : jsonArray){
-                    LookModel lookModel = gson.fromJson(jsonModel, new TypeToken<LookModel>(){}.getType());
-                    dataList.add(lookModel);
-                }
-                notifyDataSetChanged();
-            }
-        });
+       this.dataList = dataList == null ? new ArrayList<LookModel>() : dataList;
 
     }
 
@@ -98,16 +67,16 @@ public class LookAdapter extends BaseAdapter {
         View lookView = View.inflate(lContext, R.layout.activity_discover_look_adapter_item, null);
 
 
-        ImageView ivImage     = viewGroup.findViewById(R.id.lookadapter_image);  //imgsrc
-        TextView  tvTitle     = viewGroup.findViewById(R.id.lookadapter_title); //title
-        TextView  tvTag       = viewGroup.findViewById(R.id.lookadapter_tag);   //source
-        TextView  tvNumber    = viewGroup.findViewById(R.id.lookadapter_number);  //replyCount
+        ImageView ivImage     = lookView.findViewById(R.id.lookadapter_image);  //imgsrc
+        TextView  tvTitle     = lookView.findViewById(R.id.lookadapter_title); //title
+        TextView  tvTag       = lookView.findViewById(R.id.lookadapter_tag);   //source
+        TextView  tvNumber    = lookView.findViewById(R.id.lookadapter_number);  //replyCount
 
         tvTitle.setText(dataList.get(i).getTitle());
         tvTag.setText(dataList.get(i).getSource());
-        tvNumber.setText(dataList.get(i).getReplyCount());
+        String numberString = dataList.get(i).getReplyCount() == null ? "0" : dataList.get(i).getReplyCount();
+        tvNumber.setText("回帖" + numberString);
         Glide.with(lContext).load(dataList.get(i).getImgsrc()).into(ivImage);
-
         return lookView;
     }
 }
